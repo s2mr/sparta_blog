15e3f68115a2b7240795a1a5c237e47e2cf5abef
kazu <g031n085@s.iwate-pu.ac.jp>
2017-07-29 05:04:23 +0000

    created sidebar bottom

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,9 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
     @author = Author.find(1)
+    @recent_posts = Post.all.order(created_at: :desc).limit(5)
   end
 
   # GET /posts/1
